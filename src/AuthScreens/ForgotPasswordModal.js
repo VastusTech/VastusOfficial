1cf7c52485be81c06cd65f94a0ba0453a5aa26b8
Leo Belyi <leonid@ac93.org>
2018-10-31 23:37:21 -0400

    Continue auth flow

--- src/AuthScreens/ForgotPasswordModal.js
+++ src/AuthScreens/ForgotPasswordModal.js
@@ -3,14 +3,21 @@ import Semantic, { Modal, Button, Input, Image, Grid, Form, Message } from 'sema
 import Amplify, { Auth } from 'aws-amplify';
 
 class ForgotPasswordModal extends Component {
-    vastusForgotPassword() {
-        // TODO Check to see if the input fields are put  in correctly
-        Auth.forgotPassword(this.authState.username).then(function(data) {
+    state = {
+        isConfirming: false,
+        error: null
+    };
+
+    vastusForgotPassword(successHandler, failureHandler) {
+        // TODO Check to see if the input fields are put in correctly
+        Auth.forgotPassword(this.authState.username).then((data) => {
             console.log("Successfully forgot the password! :)");
             console.log(data);
-        }).catch(function(error) {
+            successHandler(data);
+        }).catch((error) => {
             console.log("Failed to forget the password (just like how I failed to forget the day my dad left me)");
             console.log(error);
+            failureHandler(error);
         });
     }
 
@@ -32,14 +39,31 @@ class ForgotPasswordModal extends Component {
         this.authState[key] = value.target.value;
         console.log("New " + key + " is equal to " + value.target.value);
     }
+
     render() {
+        function errorMessage(error) {
+            if (error) {
+                return (
+                    <Modal.Description>
+                        <Message color='red'>
+                            <h1>Error!</h1>
+                            <p>{error}</p>
+                        </Message>
+                    </Modal.Description>
+                );
+            }
+        }
+
         return(
             <Modal trigger={<Button>Forgot Password?</Button>}size='tiny'>
                 <Modal.Header>Delete Your Account</Modal.Header>
+                {errorMessage(this.state.error)}
                 <Modal.Content>
-                    <p>Are you sure you want to delete your account</p>
+                    <p>Enter your username to retrieve your information</p>
                 </Modal.Content>
                 <Modal.Actions>
+                    <Form>
+                    </Form>
                     <Button negative>No</Button>
                     <Button positive icon='checkmark' labelPosition='right' content='Yes' />
                 </Modal.Actions>
