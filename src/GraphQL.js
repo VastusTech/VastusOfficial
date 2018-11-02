38a57b856f0bd120b0d36dbc18c17873207de3ab
Leo Belyi <leonid@ac93.org>
2018-11-02 00:07:20 -0400

    make graphQL easier

--- src/GraphQL.js
+++ src/GraphQL.js
@@ -5,31 +5,31 @@ class GraphQL {
     static getClient(id, variableList, successHandler, failureHandler) {
         const parameters = {id: id};
         this.execute(this.constructQuery("GetClient", "getClient", parameters, variableList),
-            parameters, successHandler, failureHandler);
+            "getClient", parameters, successHandler, failureHandler);
     }
     static getClientByUsername(username, variableList, successHandler, failureHandler) {
         const parameters = {username: username};
         this.execute(this.constructQuery("GetClientByUsername", "getClientByUsername", parameters, variableList),
-            parameters, successHandler, failureHandler);
+            "getClientByUsername", parameters, successHandler, failureHandler);
     }
     static getParty(id, variableList, successHandler, failureHandler) {
         const parameters = {id: id};
         this.execute(this.constructQuery("GetParty", "getParty", parameters, variableList),
-            parameters, successHandler, failureHandler);
+            "getParty", parameters, successHandler, failureHandler);
     }
     static getChallenge(id, variableList, successHandler, failureHandler) {
         const parameters = {id: id};
         this.execute(this.constructQuery("GetChallenge", "getChallenge", parameters, variableList),
-            parameters, successHandler, failureHandler);
+            "getChallenge", parameters, successHandler, failureHandler);
     }
     // TODO Eventually make this work better to allow for more intelligent queries
     static queryChallenges(variableList, successHandler, failureHandler) {
         this.execute(this.constructQuery("QueryChallenges", "queryChallenges", {}, variableList, true),
-            {}, successHandler, failureHandler);
+            "queryChallenges", {}, successHandler, failureHandler);
     }
     static queryClients(variableList, successHandler, failureHandler) {
         this.execute(this.constructQuery("QueryClients", "queryClients", {}, variableList, true),
-            {}, successHandler, failureHandler);
+            "queryClients", {}, successHandler, failureHandler);
     }
     // TODO This only supports input String! types. Reason to change?
     static constructQuery(queryName, queryFunction, inputVariables, outputVariables, ifList = false) {
@@ -75,11 +75,15 @@ class GraphQL {
         query += '    }\n}';
         return query;
     }
-    static async execute(query, variables, successHandler, failureHandler) {
+    static async execute(query, queryFunctionName, variables, successHandler, failureHandler) {
         alert("About to send to GraphQL: " + query);
         API.graphql(graphqlOperation(query, variables)).then((data) => {
             console.log("GraphQL operation succeeded!");
-            successHandler(data);
+            if (!data.data || !data.data[queryFunctionName]) {
+                console.log("Object returned nothing");
+                failureHandler("Object had returned null");
+            }
+            successHandler(data.data[queryFunctionName]);
         }).catch((error) => {
             console.log("GraphQL operation failed...");
             failureHandler(error);
