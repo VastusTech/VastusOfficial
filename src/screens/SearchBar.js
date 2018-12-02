6bd769a1cec8cd4bd1fcb37caf259ee830f0a8e0
Blake Hatch <blakewihatch@gmail.com>
2018-12-02 16:57:50 -0500

    Added correct placeholder for search bar.

--- src/screens/SearchBar.js
+++ src/screens/SearchBar.js
@@ -216,7 +216,7 @@ class SearchBarProp extends Component {
                 <Search
                     fluid
                     size="large"
-                    defaultValue={"Search for Users and Events!"}
+                    placeholder="Search for Users and Events"
                     loading={isLoading}
                     onResultSelect={this.handleResultSelect}
                     onSearchChange={_.debounce(this.handleSearchChange, 500, { leading: true })}
