64f5e864b46c7a88742132e7cd203f3f6a18a3fc
Jesse Brack <jtbrack@gmail.com>
2018-12-10 14:22:41 -0500

    Center utility

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -141,7 +141,7 @@ class Profile extends React.PureComponent {
             //     );
             // }
             return(
-                <div className="u-avatar u-avatar--large u-margin-top--neg4" style={{backgroundImage: `url(${this.props.user.profilePicture})`}}>
+                <div className="u-avatar u-avatar--large u-margin-x--auto u-margin-top--neg4" style={{backgroundImage: `url(${this.props.user.profilePicture})`}}>
                     <Label as="label" htmlFor="proPicUpload" circular className="u-bg--primaryGradient">
                         <Icon name="upload" className='u-margin-right--0' size="large" inverted />
                     </Label>
