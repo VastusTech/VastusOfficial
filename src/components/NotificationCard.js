fa5562f5018cfc989c4dfa3be43a05bf4be80c9f
Jesse Brack <jtbrack@gmail.com>
2018-12-12 18:00:50 -0500

    Testing out different profile card layout

--- src/components/NotificationCard.js
+++ src/components/NotificationCard.js
@@ -278,35 +278,26 @@ class NotificationCard extends Component {
             if (this.getInviteAttribute("inviteType") === "friendRequest") {
                 return (
                     <Card fluid raised>
+                        <Image src={this.getFromAttribute("profilePicture")} />
                         <Card.Content>
-                            <Feed>
-                                <Feed.Event>
-                                    <Feed.Label>
-                                        <Image src={this.getFromAttribute("profilePicture")} circular size="large"/>
-                                    </Feed.Label>
-                                    <Feed.Content>
-                                        <Feed.Summary>
-                                            <Feed.User onClick={this.handleClientModalOpen.bind(this)}>
-                                                {this.getFromAttribute("name")}
-                                            </Feed.User>
-                                            <ClientModal
-                                                clientID={this.getAboutAttribute("id")}
-                                                open={this.state.clientModalOpen}
-                                                onOpen={this.handleClientModalOpen.bind(this)}
-                                                onClose={this.handleClientModalClose.bind(this)}
-                                            />
-                                            {' '}has sent you a buddy request
-                                            <Feed.Date>{/*Insert Invite Sent Time Here*/}</Feed.Date>
-                                        </Feed.Summary>
-                                        <Divider/>
-                                        <Feed.Extra>
-                                            <Button inverted floated="right" size="small" onClick={this.handleDeclineFriendRequestButton.bind(this)}>Deny</Button>
-                                            <Button primary floated="right" size="small" onClick={this.handleAcceptFriendRequestButton.bind(this)}>Accept</Button>
-                                        </Feed.Extra>
-                                    </Feed.Content>
-                                </Feed.Event>
-                            </Feed>
+                            <Card.Header onClick={this.handleClientModalOpen.bind(this)}>
+                                {this.getFromAttribute("name")}
+                            </Card.Header>
+                            <Card.Description>
+                            has sent you a buddy request {/*Insert Invite Sent Time Here*/}
+                            </Card.Description>
                         </Card.Content>
+                        <Card.Content extra>
+                            
+                            <Button inverted floated="right" size="small" onClick={this.handleDeclineFriendRequestButton.bind(this)}>Deny</Button>
+                            <Button primary floated="right" size="small" onClick={this.handleAcceptFriendRequestButton.bind(this)}>Accept</Button>     
+                        </Card.Content>
+                        <ClientModal
+                            clientID={this.getAboutAttribute("id")}
+                            open={this.state.clientModalOpen}
+                            onOpen={this.handleClientModalOpen.bind(this)}
+                            onClose={this.handleClientModalClose.bind(this)}
+                        />
                     </Card>
                 );
             }
