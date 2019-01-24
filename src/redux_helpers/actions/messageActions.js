601ef01701a89f79b7a6d986864c8a4d378bbb74
Leo Belyi <leonid@ac93.org>
2019-01-24 12:28:44 -0500

    Add profile images for messaging

--- src/redux_helpers/actions/messageActions.js
+++ src/redux_helpers/actions/messageActions.js
@@ -3,6 +3,9 @@ import S3 from "../../api/S3Storage";
 import {setError, setIsLoading, setIsNotLoading} from "./infoActions";
 import {consoleError} from "../../logic/DebuggingHelper";
 
+const notFoundPicture = require('../../img/not_found.png');
+const defaultProfilePicture = require("../../img/roundProfile.png");
+
 const ADD_MESSAGE = 'ADD_MESSAGE';
 const ADD_QUERY = 'ADD_QUERY';
 const CLEAR_BOARD = 'CLEAR_BOARD';
@@ -16,14 +19,18 @@ export function queryNextMessagesFromBoard(board, limit, dataHandler, failureHan
         // console.log("IF FIRST = " + ifFirst + ", NEXT TOKEN = " + nextToken);
         if (ifFirst || nextToken) {
             // Then you do the query
-            QL.queryMessages(QL.constructMessageQuery(board, ["from", "name", "message", "type", "board", "id", "time_created"], null, limit, nextToken), (data) => {
+            QL.queryMessages(QL.constructMessageQuery(board, ["from", "name", "profileImagePath", "message", "type", "board", "id", "time_created"], null, limit, nextToken), (data) => {
                 if (data) {
                     // console.log(JSON.stringify(data));
                     if (!data.items) { data.items = []; }
-                    addURLToMessages(data.items, (messages) => {
-                        dispatch(addQueryToBoard(board, messages, data.nextToken));
-                        if (dataHandler) { dataHandler(messages); }
-                        dispatch(setIsNotLoading());
+                    addURLToMessages(data.items, "message", "messageURL", notFoundPicture, (message) => {return message.type}, (messages) => {
+                        addURLToMessages(data.items, "profileImagePath", "profilePicture", defaultProfilePicture, (message) => {return message.profileImagePath}, (messages) => {
+                            dispatch(addQueryToBoard(board, messages, data.nextToken));
+                            if (dataHandler) {
+                                dataHandler(messages);
+                            }
+                            dispatch(setIsNotLoading());
+                        });
                     });
                 }
                 else {
@@ -46,14 +53,26 @@ export function queryNextMessagesFromBoard(board, limit, dataHandler, failureHan
         }
     };
 }
-function addURLToMessages(messages, dataHandler) {
+
+/**
+ * This function takes in a list of messages and fetches all of their image attributes from AWS S3.
+ *
+ * @param messages The list of messages to fetch the image attribute of.
+ * @param messagePathField The field in the Message object that contains the S3 path for the image.
+ * @param messageURLField The field in the Message object to put the URL in.
+ * @param defaultURL The default URL to put into the URL field (if fails or not applicable).
+ * @param fetchChecker The checker that takes in a Message object and returns a boolean for if the message
+ * should be fetched
+ * @param dataHandler The handler at the end of the function that returns the newly fetched messages array.
+ */
+function addURLToMessages(messages, messagePathField, messageURLField, defaultURL, fetchChecker, dataHandler) {
     const messagesLength = messages.length;
     let messagesReturned = 0;
     for (let i = 0; i < messagesLength; i++) {
         const message = messages[i];
-        if (message.type) {
-            S3.get(message.message, (url) => {
-                message.message = url;
+        if (fetchChecker(message)) {
+            S3.get(message[messagePathField], (url) => {
+                message[messageURLField] = url;
                 messagesReturned++;
                 if (messagesReturned >= messagesLength) {
                     dataHandler(messages);
@@ -61,7 +80,7 @@ function addURLToMessages(messages, dataHandler) {
             }, (error) => {
                 consoleError("FAILED TO RECEIVE URL FOR MEDIA IN MESSAGE! ERROR = " + JSON.stringify(error));
                 messagesReturned++;
-                message.message = "";
+                message[messageURLField] = defaultURL;
                 if (messagesReturned >= messagesLength) {
                     dataHandler(messages);
                 }
@@ -69,6 +88,7 @@ function addURLToMessages(messages, dataHandler) {
         }
         else {
             messagesReturned++;
+            message[messageURLField] = defaultURL;
             if (messagesReturned >= messagesLength) {
                 dataHandler(messages);
             }
