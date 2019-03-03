cde44fc065fd3854f38e592b8f9fafe2affcafe0
Blake Hatch <blakewihatch@gmail.com>
2019-03-03 00:39:38 -0500

    Added attribute getter to message board feed

--- src/screens/messaging_tab/MessageBoardCard.js
+++ src/screens/messaging_tab/MessageBoardCard.js
@@ -2,31 +2,22 @@ import React from 'react';
 import { Card, Grid, Header } from "semantic-ui-react";
 
 type Props = {
-    messageBoardID: string
+    messageBoardID: string,
+    otherBoardUserName: string,
 };
 
 const MessageBoardCard = (props: Props) => {
     return (
-        <Card fluid raised onClick={() => this.openClientModal()}>
+        <Card fluid raised onClick={() => this.props.openClientModal()}>
             <Card.Content>
                 <Grid divided verticalAlign='middle'>
                     <Grid.Row>
                         <Grid.Column width={4}>
-                            <Header size='large' textAlign='center'>{props.messageBoardID}</Header>
+                            <Header size='large' textAlign='center'>{props.otherBoardUserName}</Header>
                         </Grid.Column>
-                        {/*<Grid.Column width={12}>*/}
-                            {/*<div className="u-flex u-flex-align--center">*/}
-                                {/*{this.profilePicture()} <Header size='small' className='u-margin-top--0 u-margin-left--2'>{this.getClientAttribute("name")}</Header>*/}
-                            {/*</div>*/}
-                        {/*</Grid.Column>*/}
                     </Grid.Row>
                 </Grid>
             </Card.Content>
-            {/*<Card.Content extra>*/}
-                {/*<Card.Meta>*/}
-                    {/*{this.getClientAttribute("challengesWonLength")} challenges won*/}
-                {/*</Card.Meta>*/}
-            {/*</Card.Content>*/}
         </Card>
     );
 };
