b370e92748351f55a5fb675294090c79a46d7658
Leo Belyi <leonid@ac93.org>
2019-03-23 18:08:08 -0400

    Refactor like a guy

--- src/screens/main_tab/MainTab.js
+++ src/screens/main_tab/MainTab.js
@@ -1,1 +1,33 @@
-// TODO Use this so that our structure is more scalable (as opposed to just putting PostFeed.js in there)
+import React from "react";
+import {Header} from "semantic-ui-react";
+import PostFeedProp from "../../vastuscomponents/components/feeds/PostFeed";
+import NextChallengeProp from "./NextChallenge";
+import QL from "../../vastuscomponents/api/GraphQL";
+
+const MainTab = () => {
+    return [
+        <Header sub>Your Next Challenge:</Header>,
+        <NextChallengeProp/>,
+        <Header sub>Upcoming Posts:</Header>,
+        <PostFeedProp filter={
+            QL.generateFilter({
+                and: [{
+                    or: [{
+                        postType: {
+                            eq: "$postType1"
+                        }
+                    }, {
+                        postType: {
+                            eq: "$postType2"
+                        }
+                    }]
+                }
+            ]}, {
+                postType1: "Challenge",
+                postType2: "newChallenge",
+            })
+        }/>
+    ];
+};
+
+export default MainTab;
