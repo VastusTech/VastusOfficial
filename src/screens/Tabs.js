0dc76ad52c923aae53999815b03507f5d5b4f32a
Leo Belyi <leonid@ac93.org>
2019-03-22 21:18:50 -0400

    Continue functionalizing

--- src/screens/Tabs.js
+++ src/screens/Tabs.js
@@ -4,7 +4,7 @@ import NotificationFeed from "./messaging_tab/NotificationBellFeed";
 import ProfileTab from "./profile_tab/ProfileTab";
 import React from "react";
 import TrainerTab from "./trainer_tab/TrainerTab";
-import NotificationBellProp from "./messaging_tab/NotificationBell";
+import NotificationBellProp from "../vastuscomponents/components/props/NotificationBell";
 import NotificationsTab from "./messaging_tab/NotificationsTab";
 import PostFeedProp from "./main_tab/PostFeed";
 import MessageTab from "./messaging_tab/MessageTab";
