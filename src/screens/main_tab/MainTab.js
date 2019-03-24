5363f4d9ec0722976961789563dc07c27b373791
Leo Belyi <leonid@ac93.org>
2019-03-24 01:48:51 -0400

    Performance I guess and change a lot of functional components

--- src/screens/main_tab/MainTab.js
+++ src/screens/main_tab/MainTab.js
@@ -5,29 +5,31 @@ import NextChallengeProp from "../../vastuscomponents/components/info/NextChalle
 import QL from "../../vastuscomponents/api/GraphQL";
 
 const MainTab = () => {
-    return [
-        <Header sub>Your Next Challenge:</Header>,
-        <NextChallengeProp/>,
-        <Header sub>Upcoming Posts:</Header>,
-        <PostFeedProp filter={
-            QL.generateFilter({
-                and: [{
-                    or: [{
-                        postType: {
-                            eq: "$postType1"
-                        }
-                    }, {
-                        postType: {
-                            eq: "$postType2"
-                        }
-                    }]
-                }
-            ]}, {
-                postType1: "Challenge",
-                postType2: "newChallenge",
-            })
-        }/>
-    ];
+    return (
+        <div>
+            <Header sub>Your Next Challenge:</Header>
+            <NextChallengeProp/>
+            <Header sub>Upcoming Posts:</Header>
+            <PostFeedProp filter={
+                QL.generateFilter({
+                    and: [{
+                        or: [{
+                            postType: {
+                                eq: "$postType1"
+                            }
+                        }, {
+                            postType: {
+                                eq: "$postType2"
+                            }
+                        }]
+                    }
+                ]}, {
+                    postType1: "Challenge",
+                    postType2: "newChallenge",
+                })
+            }/>
+        </div>
+    );
 };
 
 export default MainTab;
--- src/screens/trainer_tab/CommunityTab.js
+++ src/screens/trainer_tab/CommunityTab.js
@@ -1,146 +1,22 @@
-import React, { Component, Fragment } from "react";
-import { connect } from "react-redux";
+import React from "react";
 import {Grid, Button, Modal} from 'semantic-ui-react'
-import {fetchUserAttributes} from "../../redux_helpers/actions/userActions";
 import CreateGroupProp from "../../vastuscomponents/components/manager/CreateGroup";
-import GroupCard from "../../vastuscomponents/components/cards/GroupCard";
-import {
-    fetchClient,
-    fetchGroupQuery,
-    fetchGroup,
-    fetchTrainer,
-    putGroup,
-    putGroupQuery,
-} from "../../vastuscomponents/redux_actions/cacheActions";
-import _ from "lodash";
-import {err} from "../../Constants";
+import GroupFeed from "../../vastuscomponents/components/feeds/GroupFeed";
+import QL from "../../vastuscomponents/api/GraphQL";
 
-class CommunityTab extends Component {
-    state = {
-        isLoading: true,
-        userID: null,
-        groups: [],
-        groupFeedLength: 10,
-        sentRequest: false,
-        nextToken: null,
-        ifFinished: false,
-        calculations: {
-            topVisible: false,
-            bottomVisible: false
-        },
-    };
-
-    constructor(props) {
-        super(props);
-        this.queryGroups = this.queryGroups.bind(this);
-    }
-
-    componentDidMount() {
-    }
-
-    componentWillReceiveProps(newProps, nextContext) {
-        if (this.state.userID !== newProps.userID) {
-            this.setState({userID: newProps.userID});
-            this.queryGroups(newProps.userID);
-        }
-    }
-
-    queryGroups() {
-        // console.log("BEFORE: " + this.state.sentRequest);
-        if (!this.state.sentRequest) {
-            this.state.sentRequest = true;
-            // console.log("AFTER: " + this.state.sentRequest);
-            if (!this.state.ifFinished) {
-                this.setState({isLoading: true});
-                this.props.fetchGroupQuery(GroupCard.fetchVariableList, null, this.state.groupFeedLength, this.state.nextToken, (data) => {
-                    if (!data.nextToken) {
-                        this.setState({ifFinished: true});
-                    }
-                    if (data.items) {
-                        for (let i = 0; i < data.items.length; i++) {
-                            const group = data.items[i];
-
-                            // TODO Fetch all the information that we need to about the groups
-
-                            this.state.groups.push(group);
-                        }
-                        this.setState({nextToken: data.nextToken});
-                    }
-                    else {
-                        // TODO Came up with no events
-                    }
-                    this.setState({isLoading: false});
-                }, (error) => {
-                    err&&console.error("Querying Groups failed!");
-                    err&&console.error(error);
-                    this.setState({isLoading: false, error: error});
-                }, this.props.cache.groupQueries, this.props.putGroupQuery);
-            }
-        }
-    }
-
-    render() {
-        /**
-         * This function takes in a list of Groups and displays them in a list of Event Card views.
-         * @param Groups
-         * @returns {*}
-         */
-        function rows(groups) {
-            // if(Posts != null && Posts.length > 0)
-            return _.times(groups.length, i => (
-                <Fragment key={i + 1}>
-                    <GroupCard groupID={groups[i].id}/>
-                </Fragment>
-            ));
-        }
-
-        return (
-            <Grid fluid centered>
-                <Grid.Row height={20} style={{marginBottom: '15px', marginTop: '15px'}}>
-                <Modal trigger={<Grid centered><Button primary>Create New Group</Button></Grid>}>
-                    <CreateGroupProp/>
-                </Modal>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid centered>
-                        {rows(this.state.groups)}
-                    </Grid>
-                </Grid.Row>
+const CommunityTab = () => (
+    <Grid fluid centered>
+        <Grid.Row height={20} style={{marginBottom: '15px', marginTop: '15px'}}>
+        <Modal trigger={<Grid centered><Button primary>Create New Group</Button></Grid>}>
+            <CreateGroupProp/>
+        </Modal>
+        </Grid.Row>
+        <Grid.Row>
+            <Grid centered>
+                <GroupFeed filter={null}/>
             </Grid>
-        );
-    }
-}
+        </Grid.Row>
+    </Grid>
+);
 
-const mapStateToProps = (state) => ({
-    user: state.user,
-    info: state.info,
-    cache: state.cache,
-});
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        fetchUserAttributes: (variablesList) => {
-            dispatch(fetchUserAttributes(variablesList));
-        },
-        fetchClient: (id, variablesList, dataHandler) => {
-            dispatch(fetchClient(id, variablesList, dataHandler));
-        },
-        fetchTrainer: (id, variablesList, dataHandler) => {
-            dispatch(fetchTrainer(id, variablesList, dataHandler));
-        },
-        fetchGroup: (id, variablesList, dataHandler) => {
-            dispatch(fetchGroup(id, variablesList, dataHandler));
-        },
-        putGroup: (event) => {
-            dispatch(putGroup(event));
-        },
-        putGroupQuery: (queryString, queryResult) => {
-            dispatch(putGroupQuery(queryString, queryResult));
-        },
-        fetchGroupQuery: (variablesList, filter, limit, nextToken, dataHandler, failureHandler) => {
-            dispatch(fetchGroupQuery(variablesList, filter, limit, nextToken, dataHandler, failureHandler));
-        },
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(CommunityTab);
+export default CommunityTab;
