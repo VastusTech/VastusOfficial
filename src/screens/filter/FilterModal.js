aee8a5eeaa62988bfe564830ec02a4d86466a287
Leo Belyi <leonid@ac93.org>
2019-03-30 16:13:26 -0400

    Refactor Filter Modal slightly

--- src/screens/filter/FilterModal.js
+++ src/screens/filter/FilterModal.js
@@ -1,109 +1,16 @@
-import React, {useState, useEffect, Fragment} from 'react';
-import {connect} from 'react-redux';
+import React from 'react';
 import {Grid, Modal, Checkbox} from "semantic-ui-react";
-import {disableType, enableType} from "../../vastuscomponents/redux_actions/searchActions";
+import FilterScreen from "../../vastuscomponents/components/props/FilterScreen";
 
 type Props = {
     open: boolean,
     onClose: () => void
 };
 
-const getCheckBoxes = (filterTypes, setFilterTypes, enableType, disableType) => {
-    const checkBoxes = [];
-    for (const type in filterTypes) {
-        if (filterTypes.hasOwnProperty(type)) {
-            checkBoxes.push(
-                <div key={type}>
-                    <Grid.Column>
-                        <Checkbox label={type} toggle
-                                  checked={filterTypes[type]}
-                                  onChange={() => toggleTypeCheckbox(type, setFilterTypes, enableType, disableType)}/>
-                    </Grid.Column>
-                </div>
-            )
-        }
-    }
-    return checkBoxes;
-};
+const FilterModal = (props: Props) => (
+    <Modal open={props.open} onClose={props.onClose} closeIcon>
+        <FilterScreen/>
+    </Modal>
+);
 
-const toggleTypeCheckbox = (type, setFilterTypes, enableType, disableType) => {
-    setFilterTypes(p => {
-        if (p[type]) {
-            // Disable type
-            alert("disabling " + type);
-            disableType(type);
-        }
-        else {
-            // Enable type
-            alert("enabling " + type);
-            enableType(type);
-        }
-        return {
-            ...p,
-            [type]: !p[type]
-        }
-    });
-    // alert(JSON.stringify(event.target.checked))
-};
-
-/**
- * The Modal to filter out results from the search bar. Chooses which item types to include in the actual search
- * functionality.
- *
- * @param props The props to put into the functional component.
- * @return {*} The rendered component.
- * @constructor
- */
-const FilterModal = (props: Props) => {
-    const [filterTypes, setFilterTypes] = useState({});
-
-    useEffect(() => {
-        if (props.open) {
-            setFilterTypes({});
-            const types = props.search.typeQueries;
-            for (const type in types) {
-                if (types.hasOwnProperty(type)) {
-                    if (["Client", "Trainer", "Event", "Challenge", "Group"].includes(type)) {
-                        setFilterTypes(p => ({
-                            ...p,
-                            [type]: types[type].enabled
-                        }));
-                    }
-                }
-            }
-        }
-    }, [props.open, props.search]);
-
-    return (
-        <Modal open={props.open} onClose={props.onClose} closeIcon>
-            <Modal.Header>Filter</Modal.Header>
-            <Modal.Description>
-                Choose which item types show up in the search bar!
-            </Modal.Description>
-            <Modal.Actions>
-                <Fragment>
-                    <Grid stackable stretched columns={9}>
-                        {getCheckBoxes(filterTypes, setFilterTypes, props.enableType, props.disableType)}
-                    </Grid>
-                </Fragment>
-            </Modal.Actions>
-        </Modal>
-    );
-};
-
-const mapStateToProps = state => ({
-    search: state.search
-});
-
-const mapDispatchToProps = dispatch => {
-    return {
-        enableType: (type) => {
-            dispatch(enableType(type));
-        },
-        disableType: (type) => {
-            dispatch(disableType(type));
-        }
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(FilterModal);
+export default FilterModal;
