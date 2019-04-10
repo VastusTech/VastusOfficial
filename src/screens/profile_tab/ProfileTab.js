6a62171dd95774d8ed640df73923ce6e36d120ca
Blake Hatch <blakewihatch@gmail.com>
2019-04-10 13:31:11 -0400

    Added back in profile view

--- src/screens/profile_tab/ProfileTab.js
+++ src/screens/profile_tab/ProfileTab.js
@@ -1,10 +1,45 @@
-import React, {useState, useEffect} from 'react'
+import React, {useState} from 'react'
 import {connect} from "react-redux";
-import {Button, Card, Modal, Dimmer, Loader, List, Icon, Label, Divider, Image, Grid, Visibility} from 'semantic-ui-react'
+import {Button, Card, Modal, Dimmer, Loader, Input, Icon, Grid, Popup} from 'semantic-ui-react'
 import ChallengeList from "../../vastuscomponents/components/lists/ChallengeList";
 import DatabaseObjectList from "../../vastuscomponents/components/lists/DatabaseObjectList";
 import ProfileImage from "../../vastuscomponents/components/props/ProfileImage";
 import LogOutButton from "../../vastuscomponents/components/manager/LogOutButton";
+
+function displayName(isEditing, name) {
+    if(!isEditing) {
+        return (
+            <Card.Header as="h2" style={{"margin": "12px 0 0"}}>{name}</Card.Header>
+        );
+    }
+    else {
+        return (
+            <Input defaultValue={name}/>
+        );
+    }
+}
+
+function editButton(isEditing, setIsEditing) {
+    if(!isEditing) {
+        return (
+            <Button onClick = { () => setIsEditing(p => !p)} floated='left' circular icon color={'purple'}>
+                <Icon name='edit outline'/>
+            </Button>
+        );
+    }
+    else {
+        return (
+            <div>
+                <Button onClick = { () => setIsEditing(p => !p)} floated='left' circular icon color={'purple'}>
+                    <Icon name='save'/>
+                </Button>
+                <Button onClick = { () => setIsEditing(p => !p)} floated='left' circular icon>
+                    <Icon name='cancel'/>
+                </Button>
+            </div>
+        );
+    }
+}
 
 /**
 * ProfileTab
@@ -12,6 +47,8 @@ import LogOutButton from "../../vastuscomponents/components/manager/LogOutButton
 * This is the profile page which displays information about the current user.
  */
 const ProfileTab = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+
     if (!props.user) {
         return (
             <Dimmer>
@@ -22,85 +59,73 @@ const ProfileTab = (props) => {
     return (
         <Card fluid raised className="u-margin-top--2">
             <Card.Content textAlign="center">
-                <ProfileImage userID={props.user.id} profileImage={props.user.profileImage} profileImagePaths={props.user.profileImagePaths} profileImages={props.user.profileImages} editable={true}/>
+                {editButton(isEditing, setIsEditing)}
+                <Popup
+                    trigger={<Button floated='right' circular icon color={'purple'}>
+                        <Icon name='cog'/>
+                    </Button>}
+                    content={<LogOutButton/>}
+                    on='click'
+                    position='bottom right'
+                />
+                <ProfileImage userID={props.user.id} profileImage={props.user.profileImage} profileImagePaths={props.user.profileImagePaths} profileImages={props.user.profileImages} editable={isEditing}/>
+                {displayName(isEditing, props.user.name)}
                 <Card.Header as="h2" style={{"margin": "12px 0 0"}}>{props.user.name}</Card.Header>
                 <Card.Meta>Event Wins: {props.user.challengesWon ? props.user.challengesWon.length : 0}</Card.Meta>
-                <List id = "profile buttons">
-                    {/*<List.Item>*/}
-                        {/*<Modal closeIcon trigger={<Button primary fluid size="large"><Icon name="picture" /> Photo Gallery</Button>} >*/}
-                            {/*<ProfileImageGallery />*/}
-                        {/*</Modal>*/}
-                    {/*</List.Item>*/}
-                    <List.Item>
-                        <Modal fluid size='large' closeIcon
-                            trigger={<Button primary fluid size="large"><Icon name="users" /> Buddy List</Button>}>
-                            <Modal.Header>Buddy List</Modal.Header>
-                            <Modal.Content image>
-                                <Modal.Description>
+                <Grid columns={2} celled>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Modal fluid size='large' closeIcon
+                                   trigger={<Button primary circular icon size="large"><Icon name="users"/></Button>}>
+                                <Modal.Header>Buddy List</Modal.Header>
+                                <Modal.Content image>
+                                    <Modal.Description>
+                                        <DatabaseObjectList
+                                            ids={props.user.friends}
+                                            noObjectsMessage={"No friends yet!"}
+                                            acceptedItemTypes={["Client", "Trainer"]}
+                                            randomized
+                                        />
+                                    </Modal.Description>
+                                </Modal.Content>
+                            </Modal>
+                            <div>Buddy List</div>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Modal basic size='mini' closeIcon
+                                   trigger={<Button primary circular icon size="large"><Icon name="trophy"/></Button>}>
+                                <Modal.Content>
                                     <DatabaseObjectList
-                                        ids={props.user.friends}
-                                        noObjectsMessage={"No friends yet!"}
-                                        acceptedItemTypes={["Client", "Trainer"]}
+                                        ids={props.user.ownedChallenges}
+                                        noObjectsMessage={"No Owned Challenges yet!"}
+                                        acceptedItemTypes={["Challenge"]}
                                         randomized
-                                        // sortFunction={(a, b) => {
-                                        //     const aLen = a.challengesWon ? a.challengesWon.length : 0;
-                                        //     const bLen = b.challengesWon ? b.challengesWon.length : 0;
-                                        //     return bLen - aLen;
-                                        // }}
                                     />
-                                </Modal.Description>
-                            </Modal.Content>
-                        </Modal>
-                    </List.Item>
-                    <Divider />
-                    <List.Item>
-                        <Modal basic size='mini' closeIcon
-                            trigger={<Button primary fluid size="large"><Icon name="trophy" /> Created Challenges</Button>}>
-                            <Modal.Content>
-                                <DatabaseObjectList
-                                    ids={props.user.ownedChallenges}
-                                    noObjectsMessage={"No Owned Challenges yet!"}
-                                    acceptedItemTypes={["Challenge"]}
-                                    randomized
-                                />
-                                {/*<ChallengeList challengeIDs={props.user.ownedChallenges} noChallengesMessage={"No Owned Challenges Yet!"}/>*/}
-                            </Modal.Content>
-                        </Modal>
-                    </List.Item>
-                    <List.Item>
-                        <Modal basic size='mini' closeIcon
-                            trigger={<Button primary fluid size="large"><Icon name="checked calendar" /> Scheduled Challenges</Button>}>
-                            <Modal.Content>
-                                <DatabaseObjectList
-                                    ids={props.user.challenges}
-                                    noObjectsMessage={"No Scheduled Challenges Yet!"}
-                                    acceptedItemTypes={["Challenge"]}
-                                    randomized
-                                />
-                                {/*<ChallengeList challengeIDs={props.user.challenges} noChallengesMessage={"No Scheduled Challenges Yet!"}/>*/}
-                            </Modal.Content>
-                        </Modal>
-                    </List.Item>
-                    <List.Item>
-                        <Modal basic size='mini' closeIcon
-                            trigger={<Button fluid size="large"><Icon name="bookmark outline" />Completed Challenges</Button>}>
-                            <Modal.Content>
-                                <ChallengeList challengeIDs={props.user.completedChallenges} noChallengesMessage={"No completed challenges yet!"}/>
-                            </Modal.Content>
-                        </Modal>
-                    </List.Item>
-                    <Divider />
-                    <List.Item>
-                        <LogOutButton/>
-                    </List.Item>
-                </List>
+                                </Modal.Content>
+                            </Modal>
+                            <div>Owned Challenges</div>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Modal basic size='mini' closeIcon
+                                   trigger={<Button circular icon size="large"><Icon name="bookmark outline"/></Button>}>
+                                <Modal.Content>
+                                    <ChallengeList challengeIDs={props.user.completedChallenges}
+                                                   noChallengesMessage={"No completed challenges yet!"}/>
+                                </Modal.Content>
+                            </Modal>
+                            <div>Completed Challenges</div>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </Card.Content>
         </Card>
     );
 };
 
 const mapStateToProps = state => ({
-    user: state.user,
+    user: state.user
 });
 
 export default connect(mapStateToProps)(ProfileTab);
