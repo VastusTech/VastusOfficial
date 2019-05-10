e991d22be8c529f7fa533c078e54d578566fc924
Leo Belyi <leonid@ac93.org>
2019-05-10 14:13:06 -0400

    Allow set testing to set log and err

--- src/Constants.js
+++ src/Constants.js
@@ -1,9 +1,17 @@
 export const ifDebug = false;
-export const log = true;
-export const err = true;
+export let log = true;
+export let err = true;
 export const ifAlert = false;
 export const reduxLog = true;
 export const ifCallLambdaAtStart = true;
 
+export const setLog = l => log = l;
+export const setErr = e => err = e;
+
 // Don't touch this
 export const appUserItemType = "Client";
+
+export const userInitialFetchList = ["name", "username", "birthday", "profileImagePath",
+                        "profileImagePaths", "challengesWon", "friends", "friendRequests", "scheduledEvents",
+                        "ownedEvents", "completedEvents", "challenges", "ownedChallenges", "completedChallenges",
+                        "groups", "ownedGroups", "receivedInvites", "invitedChallenges", "messageBoards", "streaks"];
