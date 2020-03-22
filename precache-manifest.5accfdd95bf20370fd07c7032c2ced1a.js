510aed0d31c32fba39d39a8b12c41f201d0625e7
Leo Belyi <leonid@ac93.org>
2020-03-22 10:06:55 -0400

    Updates

---
+++ precache-manifest.5accfdd95bf20370fd07c7032c2ced1a.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "7396fe36c8965dcac892",
+    "url": "/VastusOfficial/static/css/main.e67f31af.chunk.css"
+  },
+  {
+    "revision": "7396fe36c8965dcac892",
+    "url": "/VastusOfficial/static/js/main.7396fe36.chunk.js"
+  },
+  {
+    "revision": "5e29a5142e637307b1ad",
+    "url": "/VastusOfficial/static/css/1.4f3c2d93.chunk.css"
+  },
+  {
+    "revision": "5e29a5142e637307b1ad",
+    "url": "/VastusOfficial/static/js/1.5e29a514.chunk.js"
+  },
+  {
+    "revision": "37510514f6608d7a22ee",
+    "url": "/VastusOfficial/static/js/runtime~main.37510514.js"
+  },
+  {
+    "revision": "798ed7fcfdf919647b6c8b275180b684",
+    "url": "/VastusOfficial/static/media/VC_logo.798ed7fc.svg"
+  },
+  {
+    "revision": "5c6863077c738d670d0880247e3965f3",
+    "url": "/VastusOfficial/static/media/vt_gold_even_thicker_border.5c686307.svg"
+  },
+  {
+    "revision": "7bc5f8093bd5f571750c91e2e3b08206",
+    "url": "/VastusOfficial/static/media/roundProfile.7bc5f809.png"
+  },
+  {
+    "revision": "29489f5ba8d62cecddcfd5f6fa116a67",
+    "url": "/VastusOfficial/static/media/vastus-tech-icons-03.29489f5b.svg"
+  },
+  {
+    "revision": "79abcccccfc38556baabac3e6ede63a7",
+    "url": "/VastusOfficial/static/media/vastus-tech-icons-04.79abcccc.svg"
+  },
+  {
+    "revision": "c1906ff5a67dc75271020fc8f56347bb",
+    "url": "/VastusOfficial/static/media/vastus-tech-icons-02.c1906ff5.svg"
+  },
+  {
+    "revision": "3cbad3997a15f3effd4cd89dc5bfdbd8",
+    "url": "/VastusOfficial/static/media/vastus-tech-icons-01.3cbad399.svg"
+  },
+  {
+    "revision": "9bb9149b7190088c89b0af5590fc25f5",
+    "url": "/VastusOfficial/static/media/Endurance_icon.9bb9149b.png"
+  },
+  {
+    "revision": "1019cd0186cf35839494388e9d6bb0dc",
+    "url": "/VastusOfficial/static/media/HIIT_icon.1019cd01.png"
+  },
+  {
+    "revision": "9f5a7a088678e74934f6dd3f23e09c88",
+    "url": "/VastusOfficial/static/media/Performance_icon.9f5a7a08.png"
+  },
+  {
+    "revision": "f874a8115d5b5cde96fe8b6e12fd47c3",
+    "url": "/VastusOfficial/static/media/Strength_icon.f874a811.png"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/VastusOfficial/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/VastusOfficial/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/VastusOfficial/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/VastusOfficial/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/VastusOfficial/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/VastusOfficial/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/VastusOfficial/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/VastusOfficial/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/VastusOfficial/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/VastusOfficial/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/VastusOfficial/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/VastusOfficial/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/VastusOfficial/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/VastusOfficial/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/VastusOfficial/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/VastusOfficial/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "40089de92d0939056ea1595b9c9b4537",
+    "url": "/VastusOfficial/static/media/vt_new.40089de9.svg"
+  },
+  {
+    "revision": "caf664f40f3a499717e7faaa6201f8a2",
+    "url": "/VastusOfficial/index.html"
+  }
+];
